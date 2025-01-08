d005942144278dddb2ca3d1ae561d2538fb48e58
xudingqiang <xudingqiang520@126.com>
2025-01-08 19:10:37 +0800

    first commit

---
+++ src/com/fde/fde_linux_app_launcher/MainActivity.java
@@ -0,0 +1,53 @@
+package com.fde.tools;
+
+import android.content.Context;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        context = this;
+
+        try {
+            String openParams = getIntent().getStringExtra("openParams");
+            String[] arrParams = openParams.split("###");
+            String name = arrParams[0].trim().replaceAll("%[FfUu]", "");
+            String exec = arrParams[1].trim().replaceAll("%[FfUu]", "");
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    String result = NetUtils.getFdeMode();
+                    if ("shell".equals(result)) {
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                NetUtils.gotoLinuxApp(name, exec);
+                            }
+                        }).start();
+                    } else {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(context, R.string.fde_app_choose, Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+                    finish();
+                }
+            }).start();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(context, R.string.fde_app_choose, Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+    }
+}
