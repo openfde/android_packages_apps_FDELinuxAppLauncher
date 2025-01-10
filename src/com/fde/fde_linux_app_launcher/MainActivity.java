cd12afd738641bb11155e05f62cd44af96d0d22a
xudingqiang <xudingqiang520@126.com>
2025-01-10 16:25:20 +0800

    fix open linux type

--- src/com/fde/fde_linux_app_launcher/MainActivity.java
+++ src/com/fde/fde_linux_app_launcher/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Toast;
 import com.fde.fde_linux_app_launcher.R;
+import android.content.Intent;
+import android.content.ComponentName;
 
 public class MainActivity extends Activity {
     Context context;
@@ -34,12 +36,19 @@ public class MainActivity extends Activity {
                             }
                         }).start();
                     } else {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(context, R.string.fde_app_choose, Toast.LENGTH_LONG).show();
-                            }
-                        });
+                        Intent intent = new Intent();
+                        ComponentName componentName = new ComponentName("com.fde.x11", "com.fde.x11.AppListActivity");
+                        intent.setComponent(componentName);
+                        intent.putExtra("App", name);
+                        intent.putExtra("vnc_activity_name", name);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        // runOnUiThread(new Runnable() {
+                        //     @Override
+                        //     public void run() {
+                        //         Toast.makeText(context, R.string.fde_app_choose, Toast.LENGTH_LONG).show();
+                        //     }
+                        // });
                     }
                     finish();
                 }
