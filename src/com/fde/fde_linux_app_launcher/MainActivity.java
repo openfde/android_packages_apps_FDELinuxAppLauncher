1e5d81ab39c91d5b4cc0fc04da09010470939b4f
xudingqiang <xudingqiang520@126.com>
2025-02-21 11:03:47 +0800

    user test code

--- src/com/fde/fde_linux_app_launcher/MainActivity.java
+++ src/com/fde/fde_linux_app_launcher/MainActivity.java
@@ -75,27 +75,27 @@ public class MainActivity extends Activity {
                         if(!isAppInstalled || isUpdate){
                             parseGitXml( context,downloadJson);
                         }else{
-                            Intent intent = new Intent();
-                            ComponentName componentName = new ComponentName("com.fde.x11", "com.fde.x11.FakeListActivity");
-                            intent.setComponent(componentName);
-                            intent.putExtra("App", name);
-                            intent.putExtra("Path", exec);
-                            intent.putExtra("vnc_activity_name", name);
-                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                            startActivity(intent);
-                            finish();
-                            // if(Utils.isXserviceRunning(context)){
-                            //     NetUtils.gotoLinuxApp(name, exec,"1001");
-                            // }else{
-                            //     Log.i("FDE","fde x11 is not running... ");
-                            //     runOnUiThread(new Runnable() {
-                            //         @Override
-                            //         public void run() {
-                            //             Toast.makeText(context, R.string.x11_not_run, Toast.LENGTH_SHORT).show(); 
-                            //         }
-                            //     });    
-                            // }
+                            // Intent intent = new Intent();
+                            // ComponentName componentName = new ComponentName("com.fde.x11", "com.fde.x11.FakeListActivity");
+                            // intent.setComponent(componentName);
+                            // intent.putExtra("App", name);
+                            // intent.putExtra("Path", exec);
+                            // intent.putExtra("vnc_activity_name", name);
+                            // intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            // startActivity(intent);
                             // finish();
+                            if(Utils.isXserviceRunning(context)){
+                                NetUtils.gotoLinuxApp(name, exec,"1001");
+                            }else{
+                                Log.i("FDE","fde x11 is not running... ");
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(context, R.string.x11_not_run, Toast.LENGTH_SHORT).show(); 
+                                    }
+                                });    
+                            }
+                            finish();
                         }
                     }
                 }
